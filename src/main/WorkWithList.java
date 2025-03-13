ce9aab2187066b0bc71ed67962682aebaeb349e1
Aidar Mautaliev <mautaliev0aidar@gmail.com>
2025-03-13 20:12:36 +0300

    first commit
    Единый коммит

--- src/main/WorkWithList.java
+++ src/main/WorkWithList.java
@@ -13,12 +13,13 @@ public class WorkWithList {
     public static void bubbleSort(int count) {
         var list = generateRandomList(count);
         System.out.println(list);
-
-        for (var i = 0; i < count - 1; i++) {
-            if (list.get(i) > list.get(i + 1)) {
-                var temp = list.get(i);
-                list.set(i, list.get(i + 1));
-                list.set(i + 1, temp);
+        for (var j = 1; j < count; j++) {
+            for (var i = 0; i < count - j; i++) {
+                if (list.get(i) > list.get(i + 1)) {
+                    var temp = list.get(i);
+                    list.set(i, list.get(i + 1));
+                    list.set(i + 1, temp);
+                }
             }
         }
 
